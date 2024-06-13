24969a466e24dd7106da584ad1f25c6b8bec10d5
Daan Leenders <daanleenders@gmail.com>
2024-06-13 08:53:20 +0200

    Initial app with Face demo

---
+++ src/Gezicht.jsx
@@ -0,0 +1,16 @@
+import "./Gezicht.css";
+
+function Gezicht() {
+  return (
+    <div className="gezicht">
+      <div className="traan"></div>
+      <div className="ogen">
+        <div className="oog links"></div>
+        <div className="oog rechts"></div>
+      </div>
+      <div className="mond"></div>
+    </div>
+  );
+}
+
+export default Gezicht;
